6604bd1b9c8e6985b691e1e8c8a3bd7653e5f3b1
Chend <chenda.me@foxmail.com>
2020-09-21 21:03:41 +0800

    添加elementUI

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 // 加载全局样式
 import './styles/index.less'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
