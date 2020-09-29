c303b5f64a4ed0cec9acd334df74873b62476a92
Chend <chenda.me@foxmail.com>
2020-09-29 22:20:46 +0800

    顶部导航栏布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -7,7 +7,23 @@
       <app-aside class="aside-menu"/>
     </el-aside>
     <el-container>
-      <el-header class="header">Header</el-header>
+      <el-header class="header">
+        <div>
+          <i class="el-icon-s-fold"></i>
+          <span>哈哈哈</span>
+        </div>
+        <el-dropdown>
+          <div class="avatar-wrap">
+            <img class="avatar" src="https://chen-test-01-img.oss-cn-beijing.aliyuncs.com/img/36dddafb-41db-402f-8caf-00e0fc5255ac.jpg" alt="image">
+            用户名
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </div>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>设置</el-dropdown-item>
+            <el-dropdown-item>注销</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <el-main class="main">
         <router-view/>
       </el-main>
@@ -42,7 +58,12 @@ export default {
   background-color: antiquewhite;
 }
 .header {
-  background-color: lightcyan;
+  // background-color: lightcyan;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #ccc;
 }
 .main {
   background-color: #E9EEF3;
@@ -56,5 +77,15 @@ export default {
 }
 .aside-menu {
   height: 100%;
+}
+.avatar-wrap {
+  display: flex;
+  align-items: center;
+  .avatar {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
 }
 </style>
