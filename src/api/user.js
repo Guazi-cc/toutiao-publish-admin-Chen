b899a8b3731abb482afd7b78f00cec22884c73c7
Chend <chenda.me@foxmail.com>
2020-10-02 22:15:22 +0800

    使用axios拦截器统一设置Token

--- src/api/user.js
+++ src/api/user.js
@@ -15,18 +15,17 @@ export const login = data => {
 }
 // 获取用户信息
 export const getUserProfile = () => {
-  const user = JSON.parse(window.localStorage.getItem('user'))
-  console.log(user)
+  // const user = JSON.parse(window.localStorage.getItem('user'))
   return request({
     method: 'GET',
-    url: '/mp/v1_0/user/profile',
+    url: '/mp/v1_0/user/profile'
     // 后端要求把需要授权的用户身份信息设置到请求头中
     // axios 可以在headers 中设置请求头
-    // 通过这种方式来模拟 Tokend 暂时写死
-    headers: {
-      // 属性名和值都要看后端接口要求
-      Authorization: `Bearer ${user.token}`
-    }
+    // 通过这种方式来模拟 Token 暂时写死
+    // headers: {
+    // 属性名和值都要看后端接口要求
+    //   Authorization: `Bearer ${user.token}`
+    // }
   })
 }
 // 修改用户信息
--- src/utils/request.js
+++ src/utils/request.js
@@ -10,6 +10,28 @@ const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
 })
 
+// 请求拦截器
+request.interceptors.request.use(
+  // 任何请求都会经过这里
+  function (config) {
+    // config 是当前请求相关的配置信息对象
+    // config 是可以修改的
+    // return 时请求才会发出去
+    const user = JSON.parse(window.localStorage.getItem('user'))
+    // 如果有用户登录信息则统一设置 Token
+    if (user) {
+      config.headers.Authorization = `Bearer ${user.token}`
+    }
+    // 允许在请求出去之前定制同意业务功能处理
+    // 例如：统一设置 Token
+
+    return config
+  }, function (error) {
+    // 请求失败会经过这里
+    return Promise.reject(error)
+  }
+)
+
 // 导出请求方法
 // 谁要使用，谁就导入该模块
 export default request
